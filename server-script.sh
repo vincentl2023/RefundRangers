c1de1a2f35af87b8bdc46ecc5ec9341fd85fb754
vincentl2023 <129344427+vincentl2023@users.noreply.github.com>
2023-05-21 11:39:29 -0500

    Create server-script.sh

---
+++ server-script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+sudo yum -y update
+sudo yum -y install ruby
+sudo yum -y install wget
+cd /home/ec2-user
+sudo chmod +x ./install
+sudo ./install auto
+sudo yum install -y python-pip
+sudo pip install awscli
+sudo dnf update -y
+sudo dnf install postgresql15
+sudo yum install php
+sudo yum install php-pgsql
+sudo yum install -y httpd
+sudo systemctl start httpd
+sudo systemctl enable httpd
