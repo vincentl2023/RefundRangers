9ce9aec5d78e4976ece8847f809487826684b644
vincentl2023 <129344427+vincentl2023@users.noreply.github.com>
2023-05-21 12:42:21 -0500

    simplifying code

--- main.tf
+++ main.tf
@@ -1,71 +1,24 @@
 provider "aws" {
-    region = "us-east-2"
+  region = "us-east-2"
 }
 
-resource "aws_iam_role" "ec2_codedeploy_role" {
-  name = "EC2CodeDeploy"
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "ec2.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-EOF
+data "aws_security_group" "default" {
+  name = "default"
 }
 
 resource "aws_instance" "web" {
-    ami = "ami-046f6df52d1930356"
-    instance_type = "t2.micro"
-    security_groups = [aws_security_group.web_traffic.name]
-    user_data = file("server-script.sh")
-    tags = {
-        Name = "Production Refund Rangers"
-    }
-}
+  ami              = "ami-046f6df52d1930356"
+  instance_type    = "t2.micro"
+  security_groups  = [data.aws_security_group.default.id]
+  user_data        = file("server-script.sh")
+  tags = {
+    Name = "Production Refund Rangers"
+  }
 
-variable "ingress" {
-    type = list(number)
-    default = [22,80,443]
-}
-
-variable "egress" {
-    type = list(number)
-    default = []
-}
-
-resource "aws_security_group" "web_traffic" {
-    name = "Allow Web Traffic"
-
-    dynamic "ingress" {
-        iterator = port
-        for_each = var.ingress
-        content {
-            from_port = port.value
-            to_port = port. value
-            protocol = "TCP"
-            cidr_blocks = ["0.0.0.0/0"]
-        }
-    }
-
-        dynamic "egress" {
-        iterator = port
-        for_each = var.egress
-        content {
-            from_port = port.value
-            to_port = port. value
-            protocol = "TCP"
-            cidr_blocks = ["0.0.0.0/0"]
-        }
-    }
+  iam_instance_profile = "EC2CodeDeploy"
 }
 
 output "PublicIP" {
-    value = aws_instance.web.public_ip
+  value = aws_instance.web.public_ip
 }
+
