cf58097a0d9abbece4850bcc93220e4191d65d59
vincentl2023 <129344427+vincentl2023@users.noreply.github.com>
2023-05-21 14:37:39 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,17 +5,13 @@ provider "aws" {
 resource "aws_instance" "web" {
   ami              = "ami-046f6df52d1930356"
   instance_type    = "t2.micro"
-  security_groups  = ["default"]  
+  security_groups  = ["default"]
   user_data        = file("server-script.sh")
   tags = {
     Name = "Production Refund Rangers"
   }
 
   iam_instance_profile = "EC2CodeDeploy"
-}
-
-output "PublicIP" {
-  value = aws_instance.web.public_ip
 }
 
 
